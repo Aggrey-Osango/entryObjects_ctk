9baaa7c87b4265595ac6f012dbee2ea03082e53a
Aggrey Osango <hurrycane661@gmail.com>
2023-09-07 21:46:49 +0300

    main app ready for development

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import customtkinter as ctk
 # from tkinter import *
 
+# intializing the app
+ctk.set_default_color_theme ("blue")
+ctk.set_appearance_mode ("light")
+
+# Initializing the ctk app obj
+app = ctk.CTk ()
+app.geometry("600x520")
+app.title ("Helium Notes")
 
 if __name__ == '__main__':
-    pass
+    app.mainloop ()
 
